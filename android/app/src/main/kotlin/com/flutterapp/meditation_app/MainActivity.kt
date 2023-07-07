739cef7eaaf8e525e6be8e9595a3bde5886043cb
gokul355 <rgokul2962@gmail.com>
2023-07-08 02:27:00 +0530

    few changes

--- android/app/src/main/kotlin/com/flutterapp/meditation_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/flutterapp/meditation_app/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.flutterapp.meditation_app
 
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity: FlutterActivity() {
+    AppCenter.start(getApplication(), "{Your app secret here}",
+    Analytics.class, Crashes.class);
 }
